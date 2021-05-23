51e0c2a125997d536d9a834911b39313614019b1
SAED ALLOUZI <saedallouzi@Samers-MacBook-Pro.local>
2021-05-23 22:49:30 +0300

    done

--- calculator.js
+++ calculator.js
@@ -1,21 +1,26 @@
-const prompt = require("prompt-sync")(); // Please don't touch me :)
+const prompt = require("prompt-sync")(); // Please don't touch me :slight_smile:
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
+const name = prompt("What's your name? ");
+console.log(Hi, I'm ${name}.);
 
+const num1 = parseInt(prompt("Please enter the first number  "));
 
-const num1 = parseInt(prompt("please enter the first number " ));
-const num2 = parseInt(prompt("please enter the second number "));
-const operation = prompt("Please choose the operation (+, -, /, *): ");
+const num2 = parseInt(prompt("Please enter the second number "));
 
+const operation = prompt("Please choose the operation (+, -, /, ) ");
 
-
-if (operation === '+')
-console.log(num1+num2)
-else if (operation === '-')
-console.log(num1-num2)
-else if (operation === '*')
-console.log(num1*num2)
-else if (operation === '/')
-console.log(num1/num2)
-else console.log("Invalid input")
+if (Number.isInteger(num1) && Number.isInteger(num2)) {
+  if (operation === "+") {
+    console.log(num1 + num2);
+  } else if (operation === "-") {
+    console.log(num1 - num2);
+  } else if (operation === "/") {
+    console.log(num1 / num2);
+  } else if (operation === "") {
+    console.log(num1 * num2);
+  } else {
+    console.log("operation is not valid. ");
+  }
+} else console.log("invalid number");
